dddc144e0783087793251913d2d920c8b62c766e
nevanaattori <jani.nevaranta@gmail.com>
2022-07-09 20:00:06 +0300

    Migrated from vue-cli to vite

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).use(router).mount("#app");
+createApp(App).mount('#app')
