4dedc8339275dcdbfa10c5424dbafb64d8cbc115
nevanaattori <jani.nevaranta@gmail.com>
2022-07-18 00:01:43 +0300

    Added additional primevue components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ConfirmationService from "primevue/confirmationservice";
 import PrimeVue from "primevue/config";
 import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
@@ -8,6 +9,7 @@ import Panel from "primevue/panel";
 import Textarea from "primevue/textarea";
 import Avatar from "primevue/avatar";
 import AvatarGroup from "primevue/avatar";
+import Menu from "primevue/menu";
 
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -16,6 +18,7 @@ import "primeicons/primeicons.css";
 const app = createApp(App);
 
 app.use(PrimeVue);
+app.use(ConfirmationService);
 
 app.component("Toolbar", Toolbar);
 app.component("Button", Button);
@@ -24,5 +27,6 @@ app.component("Textarea", Textarea);
 app.component("Dropdown", Dropdown);
 app.component("Avatar", Avatar);
 app.component("AvatarGroup", AvatarGroup);
+app.component("Menu", Menu);
 
 app.mount('#app');
