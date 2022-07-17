dc955c57beee85cce40ad9d2c95c2690b07c29e1
nevanaattori <jani.nevaranta@gmail.com>
2022-07-18 00:01:35 +0300

    Refactored const options into its own file

---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+// All constant data that can be used in the options.
+
+const dialoguePositions = [
+  "left",
+  "right"
+];
+const dialogueCharacters = [
+  "Guildmaster",
+  "Emericus",
+  "Velia",
+  "Kho",
+  "Dalton",
+  "Maryu",
+  "R.A.G.A",
+  "Solia",
+  "Pyroarix",
+];
+const dialogueTransitionEffects = [
+  "nothing",
+  "shake",
+  "jump",
+];
+
+
+export { dialoguePositions, dialogueCharacters, dialogueTransitionEffects }
