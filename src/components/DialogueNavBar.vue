25ce0fce95388c29f1f9419e31250bf774a844fa
nevanaattori <jani.nevaranta@gmail.com>
2022-07-27 20:08:56 +0300

    Navbar now contains file related tools

--- src/components/DialogueNavBar.vue
+++ src/components/DialogueNavBar.vue
@@ -1,26 +1,54 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { FileUploadRemoveEvent } from "primevue/fileupload";
+import { ref, defineProps, defineEmits, onMounted } from "vue";
 
-const items = ref([
+const exportItems = ref([
   {
-    label: "Back",
-    icon: "pi pi-arrow-left",
+    label: "Export",
+    icon: "pi pi-fw pi-eject",
   },
+  {
+    label: "Export Settings",
+    icon: "pi pi-fw pi-cog",
+    command: () => {
+      console.log("Export settings.")
+    }
+  }
 ])
+
+const props = defineProps<{
+  dialogueName: string,
+}>();
+
+const emit = defineEmits<{
+  (e: "saveFile"): void,
+  (e: "openFile"): void,
+}>()
 </script>
 
 <template>
   <nav>
     <Toolbar class="editor-navbar">
       <template #start>
-        <Button label="Back" icon="pi pi-arrow-left" />
+        <span class="p-buttonset">
+          <Button label="New" icon="pi pi-fw pi-plus" class="p-button-raised p-button-text" />
+          <Button label="Open" icon="pi pi-fw pi-file" class="p-button-raised p-button-text" @click="emit('openFile')" />
+          <Button label="Save" icon="pi pi-fw pi-save" class="p-button-raised p-button-text" @click="emit('saveFile')" />
+          <SplitButton :model="exportItems" label="Export" icon="pi pi-fw pi-eject" class="p-button-raised p-button-text" />
+        </span>
       </template>
       <template #end>
-        <Button label="Dialogue Settings" class="p-button-rounded" />
+        <Button label="Settings" icon="pi pi-fw pi-cog" class="p-button-raised p-button-text" />
       </template>
     </Toolbar>
   </nav>
 </template>
 
 <style scoped>
+.editor-navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  z-index: 1;
+}
 </style>
